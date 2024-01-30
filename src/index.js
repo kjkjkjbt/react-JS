81c16c7dcb8addfaffaeeab5cb4b60f117985bfe
BTGIT <lucywin2610@yahoo.com>
2024-01-30 10:09:21 +0700

    updatereact

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ChangeCar from './components/HandleEvent/StateDemoComponent/ChangeCar';
 
 // import HomeLayout from './components/HomeLayout/HomeLayout';
 // import Databinding from './components/Databinding/Databinding';
 // import StyleComponent from './components/StyleComponent/StyleComponent';
 // import HandleEvent from './components/HandleEvent/HandleEvent';
 // import StateDemoComponent from './components/StateDemoComponent/StateDemoComponent';
-import ChangeColor from './components/HandleEvent/StateDemoComponent/ChangeColour';
-import ChangeAvatar from './components/HandleEvent/StateDemoComponent/ChangeAvatar';
+// import ChangeColor from './components/StateDemoComponent/ChangeColor';
+// import ChangeAvatar from './components/StateDemoComponent/ChangeAvatar';
+// import ChangeCar from './components/StateDemoComponent/ChangeCar';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // jsx : => html được viết trong file js (nền js) được gọi là jsx
@@ -23,6 +26,7 @@ root.render(
     {/* <HandleEvent /> */}
     {/* <StateDemoComponent /> */}
     {/* <ChangeColor /> */}
-    <ChangeAvatar></ChangeAvatar>
+    {/* <ChangeAvatar /> */}
+    <ChangeCar />
   </div>,
 );
