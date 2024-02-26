67d07fc96589449e08cbec9ead500c1981d6fc03
BTGIT <lucywin2610@yahoo.com>
2024-02-26 20:47:09 +0700

    updated react form

--- src/Page/ReactForm/ProductForm.jsx
+++ src/Page/ReactForm/ProductForm.jsx
@@ -1,88 +1,170 @@
-    import React, { Component } from 'react';
+import React, { Component } from "react";
 
-    export default class ProductForm extends Component {
-    state = {
-        id: '',
-        tenSp: '',
-        img: '',
-        price: '',
-    };
-    handleChangeInput = (e) => {
-        // e.target đại diện cho thẻ input
+export default class ProductForm extends Component {
+  state = {
+    value: {
+      id: "",
+      tenSp: "",
+      img: "",
+      price: "",
+    },
+    errValue: {
+      id: "",
+      tenSp: "",
+      img: "",
+      price: "",
+    },
+    isSubmit: false,
+  };
+  handleChangeInput = (e) => {
+    // e.target đại diện cho thẻ input
 
-        let tag = e.target;
-        let nameInput = tag.name;
-        console.log('nameInput: ', nameInput);
-        this.setState({
-        [nameInput]: tag.value,
-        });
-    };
-    render() {
-        console.log('state', this.state);
-        return (
-        <div className="container mt-5">
-            <h2>Form Nhập Thông Tin Sản Phẩm</h2>
-            <form className="border rounded-2 p-4">
-            <div className="row">
-                <div className="col-md-6">
-                <div className="mb-3">
-                    <label htmlFor="productId" className="form-label">
-                    ID
-                    </label>
-                    <input
-                    type="text"
-                    className="form-control"
-                    name="id"
-                    id="xinchaobc64"
-                    placeholder="Nhập ID sản phẩm"
-                    onInput={this.handleChangeInput}
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="productName" className="form-label">
-                    Tên Sản Phẩm
-                    </label>
-                    <input
-                    type="text"
-                    className="form-control"
-                    name="tenSp"
-                    placeholder="Nhập tên sản phẩm"
-                    onInput={this.handleChangeInput}
-                    />
-                </div>
-                </div>
-                <div className="col-md-6">
-                <div className="mb-3">
-                    <label htmlFor="productImage" className="form-label">
-                    Hình Ảnh
-                    </label>
-                    <input
-                    type="url"
-                    className="form-control"
-                    name="img"
-                    placeholder="Nhập URL hình ảnh sản phẩm"
-                    onInput={this.handleChangeInput}
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="productPrice" className="form-label">
-                    Giá
-                    </label>
-                    <input
-                    type="number"
-                    className="form-control"
-                    name="price"
-                    placeholder="Nhập giá sản phẩm"
-                    onInput={this.handleChangeInput}
-                    />
-                </div>
-                </div>
+    let tag = e.target;
+    let dataType = e.target.getAttribute("data-type");
+    let nameInput = tag.name;
+    // clone value hiện tại
+    let newValue = { ...this.state.value };
+    // sửa tại value của key đó
+    newValue[nameInput] = tag.value;
+
+    // xử lý err
+    let newErrValue = { ...this.state.errValue };
+    let message = "";
+
+    if (newValue[nameInput] === "") {
+      message = `${nameInput} cannot be blank !`;
+    } else {
+      if (dataType) {
+        switch (dataType) {
+          case "number":
+            {
+              let regex = /^(?:[1-9]\d{0,2}|1000)$/;
+              if (!regex.test(newValue[nameInput])) {
+                message = "* Trường này chỉ nhận số";
+              }
+            }
+            break;
+          case "string":
+            {
+              let regex = /^[A-Za-z]+$/;
+              if (!regex.test(newValue[nameInput])) {
+                message = "* Trường này chỉ chữ";
+              }
+            }
+            break;
+          default: {
+          }
+        }
+      }
+    }
+    newErrValue[nameInput] = message;
+
+    // Xử lý nút submit
+    // Khi tất cả các giá trị err mà chỉ cần 1 trường có lỗi =>> lỗi
+    // chỉ 1 trường value mà "" => lỗi
+    let valid = true;
+    for (let key in newErrValue) {
+      if (newErrValue[key] !== "") {
+        valid = false;
+        break;
+      }
+    }
+    for (let key in newValue) {
+      if (newValue[key] === "") {
+        valid = false;
+        break;
+      }
+    }
+    this.setState({
+      value: newValue,
+      errValue: newErrValue,
+      isSubmit: valid,
+    });
+  };
+  render() {
+    console.log("state", this.state);
+    return (
+      <div className="container mt-5">
+        <h2>Form Nhập Thông Tin Sản Phẩm</h2>
+        <form className="border rounded-2 p-4">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="mb-3">
+                <label htmlFor="productId" className="form-label">
+                  ID
+                </label>
+                <input
+                  data-type="number"
+                  type="text"
+                  className="form-control"
+                  name="id"
+                  id="xinchaobc64"
+                  placeholder="Nhập ID sản phẩm"
+                  onInput={this.handleChangeInput}
+                />
+                <p style={{ height: "30px" }} className="text-danger">
+                  {this.state.errValue.id}
+                </p>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="productName" className="form-label">
+                  Tên Sản Phẩm
+                </label>
+                <input
+                  type="text"
+                  data-type="string"
+                  className="form-control"
+                  name="tenSp"
+                  placeholder="Nhập tên sản phẩm"
+                  onInput={this.handleChangeInput}
+                />
+                <p style={{ height: "30px" }} className="text-danger">
+                  {this.state.errValue.tenSp}
+                </p>
+              </div>
             </div>
-            <button type="submit" className="btn btn-primary">
-                Thêm Sản Phẩm
-            </button>
-            </form>
-        </div>
-        );
-    }
-    }
+            <div className="col-md-6">
+              <div className="mb-3">
+                <label htmlFor="productImage" className="form-label">
+                  Hình Ảnh
+                </label>
+                <input
+                  type="url"
+                  className="form-control"
+                  name="img"
+                  placeholder="Nhập URL hình ảnh sản phẩm"
+                  onInput={this.handleChangeInput}
+                />
+                <p style={{ height: "30px" }} className="text-danger">
+                  {this.state.errValue.img}
+                </p>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="productPrice" className="form-label">
+                  Giá
+                </label>
+                <input
+                  type="number"
+                  className="form-control"
+                  name="price"
+                  placeholder="Nhập giá sản phẩm"
+                  onInput={this.handleChangeInput}
+                />
+                <p style={{ height: "30px" }} className="text-danger">
+                  {this.state.errValue.price}
+                </p>
+              </div>
+            </div>
+          </div>
+          <button
+            disabled={!this.state.isSubmit}
+            type="submit"
+            className="btn btn-primary"
+          >
+            Thêm Sản Phẩm
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
--- src/Page/ReactForm/ReactForm.jsx
+++ src/Page/ReactForm/ReactForm.jsx
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import ProductForm from './ProductForm';
 
 export default class ReactForm extends Component {
+  state = {
+    arrProduct: [
+      {
+        id: 1,
+        name: 'Iphone',
+        img: 'https://picsum.photos/id/1/200/200',
+        price: 5000,
+      },
+      {
+        id: 2,
+        name: 'Samsung',
+        img: 'https://picsum.photos/id/2/200/202',
+        price: 7000,
+      },
+    ],
+  };
   render() {
     return (
       <div>
@@ -17,6 +33,22 @@ export default class ReactForm extends Component {
             <th>Price</th>
             <th>Action</th>
           </thead>
+          <tbody>
+            {this.state.arrProduct.map((phone) => {
+              return (
+                <tr key={phone.id}>
+                  <td>{phone.id}</td>
+                  <td>{phone.name}</td>
+                  <td>{phone.img}</td>
+                  <td>{phone.price}</td>
+                  <td>
+                    <button className="btn btn-primary">Edit</button>
+                    <button className="btn btn-success">Delete</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
         </table>
         <br />
         <br />
